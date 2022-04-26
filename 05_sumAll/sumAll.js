5d5542b09f360ed08975c0033e3385923fcb3aac
dylan <cdylan127@gmail.com>
2022-04-26 09:57:45 -0700

    Add solutions to 01 through 07

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (start, end) {
+  if (
+    typeof start !== "number" ||
+    typeof end !== "number" ||
+    start < 0 ||
+    end < 0
+  ) {
+    return "ERROR";
+  }
+  let min = start < end ? start : end;
+  let max = end > start ? end : start;
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let celsius = ((temp - 32) * 5) / 9;
+  return parseFloat((Math.round(celsius * 10) / 10).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let fahr = (temp * 9) / 5 + 32;
+  return parseFloat((Math.round(fahr * 10) / 10).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
